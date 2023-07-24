947fafd46e7785300ed0c713f2bcb085a26291f0
Diego Quijas <diego_quijassantos@hotmail.com>
2023-07-24 07:27:30 -0600

    Se agrega TodoForm

---
+++ src/App/AppUI.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoItem } from '../TodoItem';
+import { TodoList } from '../TodoList';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+import { Modal } from '../Modal';
+import { TodoForm } from '../TodoForm';
+import { TodoContext } from '../TodoContext';
+
+
+function AppUI() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+    return (
+        <>
+          <TodoCounter />
+          <TodoSearch />
+
+          <TodoList>
+          {loading && (
+            <>
+              <TodosLoading />
+              <TodosLoading />
+              <TodosLoading />
+            </>
+          )}
+          {error && <TodosError />}
+          {(!loading && searchedTodos.length === 0) && <EmptyTodos /> }
+  
+          {searchedTodos.map(todo => (
+            <TodoItem 
+              text={todo.text}
+              completed={todo.completed}
+              key={todo.text}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+          </TodoList>
+            
+    
+      <CreateTodoButton
+        setOpenModal={setOpenModal} 
+      />
+
+      {openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+
+    </>
+  );
+}
+
+export { AppUI };
